c98b3802a4ca4fc3b83696d900bacbddae08c98b
Aleksander Barczak <alex.bar074@gmail.com>
2024-01-31 01:57:12 +0000

    Added direction indicator based on which part of the grid is pressed

--- GameForm.Designer.cs
+++ GameForm.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblDirectionIndicator = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblDirectionIndicator
+            // 
+            this.lblDirectionIndicator.AutoSize = true;
+            this.lblDirectionIndicator.Font = new System.Drawing.Font("Ravie", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDirectionIndicator.Location = new System.Drawing.Point(12, 682);
+            this.lblDirectionIndicator.Name = "lblDirectionIndicator";
+            this.lblDirectionIndicator.Size = new System.Drawing.Size(325, 39);
+            this.lblDirectionIndicator.TabIndex = 0;
+            this.lblDirectionIndicator.Text = "Direction: None";
             // 
             // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(893, 848);
+            this.Controls.Add(this.lblDirectionIndicator);
             this.Name = "GameForm";
             this.Text = "Game";
             this.Load += new System.EventHandler(this.GameForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label lblDirectionIndicator;
     }
 }
--- GameForm.cs
+++ GameForm.cs
@@ -8,11 +8,14 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace AC22005Assignment1
 {
     public partial class GameForm : Form
     {
+
+        // bitmap data is currently set to 1 bit per pixel meaning the color values will either be 0 or 255, we'll expand the color values as we add more stuff
         public const int EMPTY_TILE = 255;
         public const int WALL_TILE = 0;
         public const int cellSize = 20;
@@ -58,6 +61,7 @@ namespace AC22005Assignment1
 
         public GameForm()
         {
+            Debug.WriteLine("loading");
             // initialise level data variable
             levelBitmap = new Bitmap(@"../../../level.bmp");
             levelMapData = new int[levelBitmap.Width, levelBitmap.Height];
@@ -86,15 +90,45 @@ namespace AC22005Assignment1
                     else if (levelMapData[x, y] == WALL_TILE) grid[x, y].BackColor = Color.Black;
                     else grid[x, y].BackColor = Color.Blue;
 
-                    grid[x, y].Click += new EventHandler(this.btnEvent_Click);
+                    grid[x, y].Click += new EventHandler(this.GridButtonClicked);
                     Controls.Add(grid[x, y]);
                 }
             }
         }
 
-        private void btnEvent_Click(object sender, EventArgs e)
+        private void GridButtonClicked(object sender, EventArgs e)
         {
+            // figure out center of grid
+            int centerX = cellSize * levelBitmap.Width / 2;
+            int centerY = cellSize * levelBitmap.Height / 2;
 
+            // figure out difference between center and tile pressed
+            int diffX = centerX - ((Button)sender).Location.X;
+            int diffY = centerY - ((Button)sender).Location.Y;
+
+            Debug.WriteLine("x: " + diffX.ToString() + " y: " + diffY.ToString());
+
+            // right, left or up, down
+            if (Math.Abs(diffX) > Math.Abs(diffY)) {
+                if (diffX > 0)
+                {
+                    lblDirectionIndicator.Text = "Direction: left";
+                } else
+                {
+                    lblDirectionIndicator.Text = "Direction: Right";
+                }
+            } 
+            else {
+                if (diffY > 0)
+                {
+                    lblDirectionIndicator.Text = "Direction: Up";
+                }
+                else
+                {
+                    lblDirectionIndicator.Text = "Direction: Down";
+                }
+            }
+            
         }
 
         private void GameForm_Load(object sender, EventArgs e)
